099d33f2d546da24acb99dd018ba48fc32e5d483
Raffael Scagnetti <raffael.scagnetti@bluewin.ch>
2023-04-21 06:49:05 +0200

    Add basic functionality for AGeneralCharacter
    AGeneralCharacter:
     - FieldOfView controls
     - Input restriction management
     - MotionWarping
     - BonePrefix to handle meshes with prefixes
     - General setup of the mesh

--- Source/MAProject/Private/Characters/GeneralCharacter.cpp
+++ Source/MAProject/Private/Characters/GeneralCharacter.cpp
@@ -2,13 +2,27 @@
 
 
 #include "Characters/GeneralCharacter.h"
+#include "MotionWarpingComponent.h"
 
 // Sets default values
-AGeneralCharacter::AGeneralCharacter()
+AGeneralCharacter::AGeneralCharacter() : FieldOfView(90.f)
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	MotionWarpingComponent = CreateDefaultSubobject<UMotionWarpingComponent>(TEXT("MotionWarpingComp"));
 
+	GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	GetMesh()->SetCollisionObjectType(ECC_WorldDynamic);
+	GetMesh()->SetCollisionResponseToAllChannels(ECR_Overlap);
+
+	GetMesh()->ComponentTags.Add(DamageableVolumeTag);
+	
+}
+
+void AGeneralCharacter::GetActorEyesViewPoint(FVector& OutLocation, FRotator& OutRotation) const
+{
+	if(!IsValid(GetMesh())) return Super::GetActorEyesViewPoint(OutLocation, OutRotation);
+	const FName HeadSocket = *(BonePrefix + "-HeadSocket");
+	OutLocation = GetMesh()->GetSocketLocation(HeadSocket);
+	OutRotation = GetMesh()->GetSocketRotation(HeadSocket);
 }
 
 // Called when the game starts or when spawned
@@ -16,19 +30,5 @@ void AGeneralCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-// Called every frame
-void AGeneralCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AGeneralCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
--- Source/MAProject/Private/Characters/GeneralCharacter.h
+++ Source/MAProject/Private/Characters/GeneralCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "GeneralCharacter.generated.h"
 
+class UMotionWarpingComponent;
+
 UCLASS()
 class AGeneralCharacter : public ACharacter
 {
@@ -15,15 +17,36 @@ public:
 	// Sets default values for this character's properties
 	AGeneralCharacter();
 
+	float GetFieldOfView() const { return FieldOfView; }
+
+	/**
+	 * @brief Get weather an input of the given can override the currently active one or not
+	 * @param Type the input to check
+	 * @return Weather the current input can be overridden by Type
+	 */
+	bool CanOverrideCurrentInput(EInputType Type) const { return CurrentlyAvailableInputs.CanOverrideCurrentInput(Type); }
+
+	virtual void GetActorEyesViewPoint(FVector& OutLocation, FRotator& OutRotation) const override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	FAvailableInputs CurrentlyAvailableInputs;
+	
+	//Central place to set the field of view (so these settings are done the same way for all characters)
+	UPROPERTY(EditDefaultsOnly, Category="Base Settings")
+	float FieldOfView;
 
+	//The prefix (if existent) every bone on the characters skeleton has
+	UPROPERTY(EditAnywhere, Category="Base Settings")
+	FString BonePrefix;
+
+	//TODO: maybe add physical animation component for hit and death simulations
+
+	UPROPERTY(EditAnywhere)
+	UMotionWarpingComponent* MotionWarpingComponent;
+
+	//TODO: add OnGetHit & OnGetStaggered central implementation
 };
